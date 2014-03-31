666a598b723e7455db31835675befc5d398b33fd
s <s>
2014-03-31 11:17:35 +0800

    CPP 二维索引划分步骤，为移植到CUDA做预备

--- 3Opti/cuOpti/referCPP.cpp
+++ 3Opti/cuOpti/referCPP.cpp
@@ -5,8 +5,31 @@ void matrixMul1( float* a, float*b, float*c, int n )
 {
 	for(int y=0; y<n; y++)
 		for(int x=0;x<n;x++)
+		{
 			for(int k=0;k<n;k++)
-				c[ y*n +x] += a[ y*n + k] * b[ k*n +x] ;
+			{
+				// 第一步，分配小块空间
+				float aBlock;
+				float bBlock;
+				float cBlockOne;
+
+				// 第二步，获取小块在大块中的存储位置
+				int aOffset =( y * n ) + k ;
+				int bOffset =( k * n ) + x ;
+				int cOffset =( y * n ) + x ;
+
+				// 第三步，小块赋值
+				aBlock = a[ aOffset ];
+				bBlock = b[ bOffset ];
+				cBlockOne = 0.0f;
+
+				// 第四步，小矩阵相乘
+				cBlockOne = aBlock * bBlock;
+
+				// 第五步，小矩阵相乘结果累加到大矩阵
+				c[ y*n +x] += cBlockOne ;
+			}
+		}
 }
 
 void matrixMul2( float* a, float*b, float*c, int n )
@@ -31,15 +54,17 @@ void matrixMul3( float* a, float*b, float*c, int n )
 		{
 			for(int k=0;k<n/TILE;k++)
 			{
+				// 第一步，分配小块空间
 				float aBlock[TILE][TILE];
 				float bBlock[TILE][TILE];
 				float cBlockOne[TILE][TILE];
-			
-				// 
+
+				// 第二步，获取小块在大块中的存储位置
 				int aOffset =( blockIdy * n/TILE) * (TILE*TILE) + k*TILE  ;
 				int bOffset =( k * n/TILE) * (TILE*TILE) + blockIdx*TILE  ;
 				int cOffset =( blockIdy * n/TILE) * (TILE*TILE) + blockIdx*TILE  ;
 
+				// 第三步，小块赋值
 				for(int threadIdy=0;threadIdy<TILE;threadIdy++)
 					for(int threadIdx=0;threadIdx<TILE;threadIdx++)
 						{
@@ -48,7 +73,7 @@ void matrixMul3( float* a, float*b, float*c, int n )
 							cBlockOne[threadIdy][threadIdx] = 0.0f ;
 						}
 
-
+				// 第四步，小矩阵相乘
 				for(int threadIdy=0;threadIdy<TILE;threadIdy++)
 					for(int threadIdx=0;threadIdx<TILE;threadIdx++)
 						for(int p=0;p<TILE;p++)
@@ -56,6 +81,7 @@ void matrixMul3( float* a, float*b, float*c, int n )
 						cBlockOne[threadIdy][threadIdx] += aBlock[threadIdy][p] * bBlock[p][threadIdx];
 					}
 			
+				// 第五步，小矩阵相乘结果累加到大矩阵
 				for(int threadIdy=0;threadIdy<TILE;threadIdy++)
 					for(int threadIdx=0;threadIdx<TILE;threadIdx++)
 						c[ cOffset + threadIdy*n + threadIdx] += cBlockOne[threadIdy][threadIdx];
