04ebbe9e87a9be5e340a5b53f61af58aa1f1b76c
s <s>
2014-04-03 13:00:53 +0800

    步长递减， 性能极微小提升 4%

--- 4Diy/reduction/reduction_gpu.cu
+++ 4Diy/reduction/reduction_gpu.cu
@@ -3,9 +3,30 @@
 
 #include <cuda_runtime.h>
 
-#define  BLOCKDIM	512
+#define  BLOCKDIM	256
 
 __global__ void reduction_kernel( float* array, int size )
+{
+	int bid = blockIdx.x;
+	int tid = threadIdx.x;
+	
+	if( bid*blockDim.x + tid > size )
+		return;
+
+	int offset = bid * blockDim.x;
+	float* arraySub = array + offset;
+
+	for ( int d=blockDim.x/2;d>=1; d=d/2 )
+	{
+		__syncthreads();
+		if( tid < d )
+		{
+			arraySub[tid] += arraySub[tid+d];
+		}
+	}
+}
+
+__global__ void reduction_kernel1( float* array, int size )
 {
 	int bid = blockIdx.x;
 	int tid = threadIdx.x;
@@ -42,6 +63,26 @@ float reduction_gpu( float* array, int size )
 	int sizeBlock = size>BLOCKDIM?BLOCKDIM: size;
 	int countBlock = (size+ sizeBlock-1)/sizeBlock;
 	reduction_kernel<<< countBlock, sizeBlock >>>( d_array, size );
+
+	cudaMemcpy( array, d_array, sizeof(float)*size, cudaMemcpyDeviceToHost );
+
+	cudaFree( d_array );
+
+	reduction_block( array, sizeBlock, countBlock );
+
+	return array[0];
+}
+
+float reduction_gpu1( float* array, int size )
+{
+	float* d_array ;
+	cudaMalloc( (void**)&d_array, sizeof(float)*size );
+
+	cudaMemcpy( d_array, array, sizeof(float)*size, cudaMemcpyHostToDevice );
+
+	int sizeBlock = size>BLOCKDIM?BLOCKDIM: size;
+	int countBlock = (size+ sizeBlock-1)/sizeBlock;
+	reduction_kernel1<<< countBlock, sizeBlock >>>( d_array, size );
 
 	cudaMemcpy( array, d_array, sizeof(float)*size, cudaMemcpyDeviceToHost );
 
--- 4Diy/reduction/reduction_gpu.cuh
+++ 4Diy/reduction/reduction_gpu.cuh
@@ -3,3 +3,5 @@
 void warnup_gpu( float* array, int size );
 
 float reduction_gpu( float* array, int size );
+
+float reduction_gpu1( float* array, int size );
