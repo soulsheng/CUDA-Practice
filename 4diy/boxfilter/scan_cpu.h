7f1528a5aa504a25f00fce7a6109b4a8c844065e
s <s>
2014-04-10 10:35:17 +0800

    file rename

--- 4diy/boxfilter/scan_cpu.h
+++
@@ -1,9 +0,0 @@
-
-
-float scan_cpu2( float* array, int size, int width );
-
-float scan_cpu1( float* array, int size, int width );
-
-void setArray( float* array, int size );
-
-void printArray( float* array, int size, int width ) ;
