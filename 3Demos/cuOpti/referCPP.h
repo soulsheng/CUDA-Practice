a1922f6d429d638ca1de21291ddfcd3868f22723
s <s>
2014-06-12 23:52:14 +0800

    准备整理工程——矩阵相乘diy

---
+++ 3Demos/cuOpti/referCPP.h
@@ -0,0 +1,9 @@
+
+// CPU 版本1，二维索引
+void matrixMul1( float* a, float*b, float*c, int n );
+
+// CPU 版本2，一维索引
+void matrixMul2( float* a, float*b, float*c, int n );
+
+// CPU block分块，二维索引
+void matrixMul3( float* a, float*b, float*c, int n );
