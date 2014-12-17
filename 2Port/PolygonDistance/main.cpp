d4761d5e04ac8088b0d5e93dd1e357cf0ff5fda5
s <s>
2014-12-17 16:21:21 +0800

    c programming model

--- 2Port/PolygonDistance/main.cpp
+++ 2Port/PolygonDistance/main.cpp
@@ -2,7 +2,32 @@
 #include <iostream>
 using namespace std;
 
+// Step 3: 函数声明
+void polygonDistance();
+
+
 void main()
 {
-	cout << "Polygon Distance " << endl;
+	cout << "Polygon Distance begin: " << endl;
+
+	// 编程模型：1.变量定义、2.数组申请、3.函数声明、4.函数实现、5.函数调用
+
+	cout << "Step 1: 变量定义" << endl;
+
+
+	cout << "Step 2: 数组申请" << endl;
+
+
+	cout << "Step 5: 函数调用" << endl;
+	polygonDistance();
+
+	cout << "Polygon Distance end! " << endl;
+}
+
+// Step 4: 函数实现
+void polygonDistance()
+{
+	cout << "Step 3: 函数声明" << endl;
+	cout << "Step 4: 函数实现" << endl;
+
 }
