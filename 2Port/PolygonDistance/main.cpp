86a95b46def4630f3d5d2d53ebec8a386d8a6fb2
s <s>
2014-12-17 16:34:46 +0800

    add variable, array, function parameter

--- 2Port/PolygonDistance/main.cpp
+++ 2Port/PolygonDistance/main.cpp
@@ -3,29 +3,43 @@
 using namespace std;
 
 // Step 3: 函数声明
-void polygonDistance();
+void polygonDistance( float x0, float y0, float *xi, float *yi, int n );
 
 
 void main()
 {
 	cout << "Polygon Distance begin: " << endl;
 
-	// 编程模型：1.变量定义、2.数组申请、3.函数声明、4.函数实现、5.函数调用
+	// C编程模型：1.变量定义、2.数组申请、3.函数声明、4.函数实现、5.函数调用
 
 	cout << "Step 1: 变量定义" << endl;
+	
+	// 一点 p0(x0,y0)
+	float x0 = 1.0f;
+	float y0 = 2.0f;
 
+	int n = 10000;
 
 	cout << "Step 2: 数组申请" << endl;
 
+	// 多边形顶点 pi(xi,yi)
+	float *xi = (float*)malloc( n * sizeof(float) );
+	float *yi = (float*)malloc( n * sizeof(float) );
+
+	for (int i=0;i<n;i++)
+	{
+		xi[i] = sin( (float)i/n );
+		yi[i] = cos( (float)i/n );
+	}
 
 	cout << "Step 5: 函数调用" << endl;
-	polygonDistance();
+	polygonDistance( x0, y0, xi, yi, n);
 
 	cout << "Polygon Distance end! " << endl;
 }
 
 // Step 4: 函数实现
-void polygonDistance()
+void polygonDistance( float x0, float y0, float *xi, float *yi, int n )
 {
 	cout << "Step 3: 函数声明" << endl;
 	cout << "Step 4: 函数实现" << endl;
